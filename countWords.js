97817682e81b79bccd57c280af7fde47730dd12a
Dilan Meegoda <thanudilan@ymail.com>
2024-11-21 11:08:34 +0530

    Code updates committed

--- countWords.js
+++ countWords.js
@@ -5,3 +5,17 @@ function countWords(str) {
 }
 
 countWords("JavaScript is awesome");
+
+function countWordsExt(str) {
+    str = str.trim();
+    if (str === ""){
+        return 0;
+    }
+
+    let splitWords = str.split(/\s+/);
+    let wordCount = splitWords.length;
+
+    console.log(wordCount);
+}
+
+countWordsExt("  I love coding challenges");
