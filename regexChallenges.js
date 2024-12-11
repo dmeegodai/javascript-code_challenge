a9be67e144f77875414397811efa93b404ebd7f5
Thanura <thanudilan@ymail.com>
2024-12-11 12:40:58 +0530

    Phone number validation regex code added

--- regexChallenges.js
+++ regexChallenges.js
@@ -1,3 +1,4 @@
+// Email validation
 function isValidEmail(emailAddress) {
     const regex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,}$/;
     if (regex.test(emailAddress)) {
@@ -6,4 +7,15 @@ function isValidEmail(emailAddress) {
         return "Invalid Email"
     }
 }
-console.log(isValidEmail("sexybanana-5@banana-mail.co"));
+//console.log(isValidEmail("sexybanana-5@banana-mail.co"));
+
+// Phone nummer validation
+function isValidPhoneNummer(nummer){
+    const regex = /^(\+?[0-9]{1,3})?[ -]?([0-9]{3})[ -]?([0-9]{3})[ -]?([0-9]{3})$/
+    if(regex.test(nummer)){
+        return "Valid Telefonnummer";
+    } else {
+        return "Invalid Telefonenummer";
+    }
+}
+console.log(isValidPhoneNummer("+94-772-212-625"))
