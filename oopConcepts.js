4c7b361a9ceeb1dcc4e59b6879fae7a7d2f04bf0
Thanura <thanudilan@ymail.com>
2024-12-15 10:28:27 +0530

    OOP concepts

--- oopConcepts.js
+++ oopConcepts.js
@@ -35,7 +35,87 @@ class bankAccount {
   }
 }
 
-comBank = new bankAccount(85523445361, 17000);
-console.log(comBank.checkBalance());
-console.log(comBank.depositMoney(100));
-console.log(comBank.withdrawMoney(-30));
+//comBank = new bankAccount(85523445361, 17000);
+//console.log(comBank.checkBalance());
+//console.log(comBank.depositMoney(100));
+//console.log(comBank.withdrawMoney(-30));
+
+
+
+class areaCalculator {
+#areaValue;
+
+constructor(rValue){
+  this.rValue = rValue;
+}
+
+get areaCal() {
+  return this.#areaValue = 2 * 3.14 * this.rValue;
+}
+
+}
+
+//const myCalulation =  new areaCalculator(10);
+//console.log(myCalulation.areaCal);
+
+
+class Vehicle {
+
+  constructor(origin, year) {
+    this.origin = origin;
+    this.year = year;
+  }
+
+  vehicleInfo(){
+    console.log(`This vehicle is made in ${this.origin} on ${this.year}`);
+  }
+
+}
+
+class Car extends Vehicle {
+
+  constructor(brand, origin, year) {
+    super(origin, year);
+    this.brand = brand;
+  }
+   vehicleInfo(){
+    console.log(`This car brand is ${this.brand}`);
+    super.vehicleInfo();
+   }
+
+}
+
+//const myCar = new Car("Dodge", "USA", "1969");
+//myCar.vehicleInfo();
+
+class Payment {
+  processPayment(amount){
+    throw new error("Test line added");
+  }
+}
+
+class creditCardPayemnt extends Payment{
+  processPayment(amount){
+    console.log(`Credit Card payment recieved and the amount is ${amount}`);
+  }
+}
+
+class paypalPayemnt extends Payment{
+  processPayment(amount){
+    console.log(`Paypal payment recieved and the amount is ${amount}`);
+  }
+}
+
+class onlinePayemnt extends Payment{
+  processPayment(amount){
+    console.log(`Online payment recieved and the amount is ${amount}`);
+  }
+}
+
+const myPayment =  new creditCardPayemnt();
+myPayment.processPayment(200);
+
+const myPaymentTwo = new onlinePayemnt();
+myPaymentTwo.processPayment(300);
+
+
