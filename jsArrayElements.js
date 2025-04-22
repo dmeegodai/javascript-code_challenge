c3920a101221f605a7a0b7f4d21d52b6873ab065
Thanura <thanudilan@ymail.com>
2025-04-22 08:17:46 +0530

    Committed the changes on 22-04-25

--- jsArrayElements.js
+++ jsArrayElements.js
@@ -16,4 +16,11 @@ function returnMostFrequentValue(arr) {
   return mostFrequent;
 }
 arr = [1,2,2,4,5,1,2,5,5];
-console.log(returnMostFrequentValue(arr));
+//console.log(returnMostFrequentValue(arr));
+
+function arrayConvert(arr){
+  let flattedArray = [];
+  flattedArray = arr.flat(Infinity);
+  return flattedArray;
+}
+ console.log(arrayConvert([1, 2, [3, 4], [5, [6, 7]]]));
--- jsChallenges.js
+++ jsChallenges.js
@@ -1,80 +1,82 @@
 function reverseArray(arr) {
-    const newArray = [];
+  const newArray = [];
 
-    for(let i = arr.length-1; i >=0; i--){
-        newArray.push(arr[i]);
-    }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+  }
 
-    return newArray;
-
+  return newArray;
 }
 
 //console.log(reverseArray([1,2,3,4,5]));
 
+function reverseNameArray(nameArray) {
+  const nameReversedArray = [];
+  for (let i = nameArray.length - 1; i >= 0; i--) {
+    nameReversedArray.push(nameArray[i]);
+  }
 
-function reverseNameArray(nameArray){
-
-    const nameReversedArray = [];
-    for(let i = nameArray.length -1; i >= 0; i--){
-        nameReversedArray.push(nameArray[i]);
-    };
-
-    return nameReversedArray;
+  return nameReversedArray;
 }
 
 //console.log(reverseNameArray(["Namal", "Kamal", "Amal", "Randika"]));
 
-function removeDup(arr){
-    const latestArray = [];
-    for(let i = 0; i < arr.length; i++){
-        if (!latestArray.includes(arr[i])){
-            latestArray.push(arr[i]);
-        }
-    };
-    return latestArray
+function removeDup(arr) {
+  const latestArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!latestArray.includes(arr[i])) {
+      latestArray.push(arr[i]);
+    }
+  }
+  return latestArray;
 }
 
 //console.log(removeDup([1,2,2,3,4,5,3,6]));
 //console.log(removeDup(["A","A","B","B","C"]));
 
+function greatestValue(arr) {
+  let greatValue = -Infinity;
 
-function greatestValue(arr){
-
-    let greatValue = -Infinity;
-
-    for(let i =0; i < arr.length; i++) {
-        if(arr[i] > greatValue){
-             greatValue =  arr[i];
-        } 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > greatValue) {
+      greatValue = arr[i];
     }
-    return greatValue;
+  }
+  return greatValue;
 }
 //console.log(greatestValue([1,2,2,3,4,5,3,6]));
 
-
-function createMultiplier(n){
-    let number = n;
-    function secondFuncion(y){
-
-        let finalValue = number * y;
-        return finalValue;
-    }
-    return secondFuncion;
-
+function createMultiplier(n) {
+  let number = n;
+  function secondFuncion(y) {
+    let finalValue = number * y;
+    return finalValue;
+  }
+  return secondFuncion;
 }
 //const createMultiplier2 = createMultiplier(2);
 //console.log(createMultiplier2.secondFuncion(5));
 
-
-
 // This is the code that I completed in Greentube 1st technical interview
 function reverseAction(arr) {
-    const newArry = [];
-    for(let i =arr.length-1; i >= 0; i--){
-        //newArry[0] = arr[i];
-        newArray.push(arr[i]);
-    }
-    return newArry;     
-    //arr[0] =5;   
+  const newArry = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    //newArry[0] = arr[i];
+    newArray.push(arr[i]);
+  }
+  return newArry;
+  //arr[0] =5;
 }
 //new Arry [0]= 3;
+
+function hasAnyFailures(arr) {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] >= 400) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//console.log(hasAnyFailures([200, 201, 300, 500]));
+
