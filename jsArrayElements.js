45fa042d0e4dc54cedec6811ed1974310ba5edb6
Thanura <thanudilan@ymail.com>
2025-05-13 09:55:24 +0530

    Changes committed on 13/05/25

--- jsArrayElements.js
+++ jsArrayElements.js
@@ -60,38 +60,64 @@ function findDuplicates(arr) {
 
 //console.log(findDuplicates([2,1,3,2,3,2,4,5,6,3,5]));
 
-function countStatusCodes(arr){
-    let counts = new Map();
-    for(let i = 0; i < arr.length; i++){
-      if(counts.has(arr[i])){
-        counts.set(arr[i], counts.get(arr[i]) + 1);
-      } else {
-        counts.set(arr[i], 1);
-      }
+function countStatusCodes(arr) {
+  let counts = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (counts.has(arr[i])) {
+      counts.set(arr[i], counts.get(arr[i]) + 1);
+    } else {
+      counts.set(arr[i], 1);
     }
-    return counts;
+  }
+  return counts;
 }
 //console.log(countStatusCodes([200, 200, 404, 500, 200, 401, 500, 404, 200]));
 
 function getFailures(arr) {
   //let count = new Map();
   let results = [];
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] >= 400){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 400) {
       results.push(arr[i]);
     }
-  } 
+  }
   return results;
 }
 //console.log(getFailures([200, 201, 404, 500, 502]));
 
 function indFirstFailure(arr) {
-  for(let i=0; i <= arr.length-1; i++) {
-    if(arr[i] >= 400) {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] >= 400) {
       return arr[i];
-    } 
+    }
   }
   return "No failure found";
 }
 
-console.log(indFirstFailure([200, 201, 404, 500, 502]));
+//console.log(indFirstFailure([200, 201, 404, 500, 502]));
+//console.log(indFirstFailure([200, 201, 202]));
+
+const products = [
+  { id: 1, title: "Smartphone", price: 599, rating: 4.5 },
+  { id: 2, title: "", price: 399, rating: 3.8 },
+  { id: 3, title: "Laptop", price: -1500, rating: 4.0 },
+  { id: 4, title: "Camera", price: 799, rating: 6.2 },
+  { id: 5, title: "Headphones", price: 99, rating: 4.2 },
+];
+
+function apiDataValidate(arr) {
+  let result = [];
+  for (const arrElement of arr) {
+    if (
+      arrElement.price < 0 ||
+      !arrElement.title ||
+      arrElement.rating < 0 ||
+      arrElement.rating > 5
+    ) {
+      result.push(arrElement.id);
+    }
+  }
+  return result;
+}
+
+console.log(apiDataValidate(products));
