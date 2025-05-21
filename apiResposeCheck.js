34363f2b14237957be02df33b81a6e9d9c2d35cc
Thanura <thanudilan@ymail.com>
2025-05-21 10:40:40 +0530

    Changes on 21/05/25

--- apiResposeCheck.js
+++ apiResposeCheck.js
@@ -30,8 +30,9 @@ function compareApiResponses(apiV1Response, apiV2Response) {
     }
   }
   return {
-    missingFields, newFields
-  }
+    missingFields,
+    newFields,
+  };
 }
 
 //console.log(compareApiResponses(apiV1Response, apiV2Response));
@@ -40,7 +41,7 @@ const apiV1 = {
   id: 101,
   name: "Smartphone",
   price: 299.99,
-  description: "Latest model with 5G"
+  description: "Latest model with 5G",
 };
 
 const apiV2 = {
@@ -48,40 +49,53 @@ const apiV2 = {
   name: "Smartphone",
   price: 300.02,
   description: "Latest model with 5G and AI features",
-  stock: 50
+  stock: 50,
 };
 
-compareApiResponses(apiV1, apiV2) {
+function compareApiResponses(apiV1, apiV2) {
   const finalResults = {
     missingFields: [],
     newFields: [],
     valueMismatches: [],
+  };
+  const v1Keys = Object.keys(apiV1);
+  const v2Keys = Object.keys(apiV2);
+
+  for (const key of v1Keys) {
+    if (!v2Keys.includes(key)) {
+      finalResults.missingFields.push(key);
+    }
   }
-    const v1Keys = Object.keys(apiV1);
-    const v2Keys = Object.keys(apiV2);
+  for (const key of v2Keys) {
+    if (!v1Keys.includes(key)) {
+      finalResults.newFields.push(key);
+    }
+  }
+  for (const key of v1Keys) {
+    if (v2Keys.includes(key)) {
+      const v1Value = apiV1[key];
+      const v2Value = apiV2[key];
 
-    for(const key of v1Keys){
-      if(!v2Keys.includes(key)) {
-        finalResults.missingFields.push(key);
+      if (typeof v1Value === "number" && typeof v2Value === "number") {
+        if (Math.abs(v1Value - v2Value) > 0.1) {
+          finalResults.valueMismatches.push({
+            key,
+            apiV1: v1Value,
+            apiV2: v2Value,
+          });
+        }
+      }
+      if (typeof v1Value === "string" && typeof v2Value === "string") {
+        if (!v1Value === v2Value) {
+          finalResults.valueMismatches.push({
+            key,
+            apiV1: v1Value,
+            apiV2: v2Value,
+          });
+        }
       }
     }
-    for(const key of v2Keys){
-      if(!v1Keys.includes(key)) {
-        finalResults.newFields.push(key);
-      }
-    }
-    for(const key of v1Keys){
-      if(v2Keys.includes(key)){
-        const v1Value = apiV1[key];
-        const v2Vlue = apiV2[key];
-      }
-    }
-
-
-
-
-
-
-
-
+  }
+  return finalResults;
 }
+console.log(compareApiResponses(apiV1,apiV2));
