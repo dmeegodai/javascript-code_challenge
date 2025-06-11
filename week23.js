9a5fe33c82bb5042b5dbe12dec0a30cb0fda7f07
Thanura <thanudilan@ymail.com>
2025-06-11 23:13:17 +0530

    Committed the changes on 11/06/25

--- week23.js
+++ week23.js
@@ -8,3 +8,26 @@ function removeDuplicates(arr) {
   return newArray;
 }
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+function findMax(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (newArray.length === 0) {
+      newArray.push(arr[i]);
+    } else if (arr[i] > newArray[0]) {
+      newArray = [arr[i]];
+    } else if (arr[i] === newArray[0]) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+function findMaxE(arr){
+  let max =  arr[0];
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    }
+  } return max;
+}
